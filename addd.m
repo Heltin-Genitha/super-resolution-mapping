4d3cd5c0a5c2126b1d4ca0e8936b1edf001e1d6c
Heltin-Genitha <heltingenitha@stjosephs.ac.in>
2025-03-03 23:06:15 -0800

    addd.m

--- addd.m
+++ addd.m
@@ -17,8 +17,8 @@ for r=1:1:m
         if ((b1(r,c)==255)) 
             if (I11(r,c)==0)
                 I11(r,c)=1;
-%             else
-%                 I11(r,c)=6;
+          else
+             I11(r,c)=6;
             end
         end
         if ((b2(r,c)==255)) 
@@ -35,20 +35,20 @@ for r=1:1:m
                  I11(r,c)=6;
             end
       end
-%         if ((b4(r,c)==255)) 
-%             if (I11(r,c)==0)
-%                 I11(r,c)=4;
-%             else
-%                 I11(r,c)=6;
-%             end
-%         end
-%         if ((b5(r,c)==255)) 
-%             if (I11(r,c)==0)
-%                 I11(r,c)=5;
-%             else
-%                 I11(r,c)=6;
-%             end
-%         end
+         if ((b4(r,c)==255)) 
+             if (I11(r,c)==0)
+                 I11(r,c)=4;
+             else
+                 I11(r,c)=6;
+             end
+         end
+         if ((b5(r,c)==255)) 
+             if (I11(r,c)==0)
+                 I11(r,c)=5;
+             else
+                 I11(r,c)=6;
+             end
+         end
     end
 end
 for r=1:1:m
@@ -68,21 +68,21 @@ for r=1:1:m
             b(r,c,2)=0;
             b(r,c,3)=255;%blue
         end
-%         if I11(r,c)==4
-%              b(r,c,1)=255;
-%              b(r,c,2)=255;
-%              b(r,c,3)=255;%white
-%         end
-%         if I11(r,c)==5%pink
-%             b(r,c,1)=85;
-%             b(r,c,2)=0;
-%             b(r,c,3)=155;
-%         end
-%         if ((I11(r,c)==6)||(I11(r,c)==0))%black
-%             b(r,c,1)=0;
-%             b(r,c,2)=0;
-%             b(r,c,3)=0;
-%         end
+         if I11(r,c)==4
+              b(r,c,1)=255;
+             b(r,c,2)=255;
+              b(r,c,3)=255;%white
+         end
+         if I11(r,c)==5%pink
+             b(r,c,1)=85;
+             b(r,c,2)=0;
+             b(r,c,3)=155;
+         end
+         if ((I11(r,c)==6)||(I11(r,c)==0))%black
+             b(r,c,1)=0;
+             b(r,c,2)=0;
+             b(r,c,3)=0;
+         end
 
      end
 end
